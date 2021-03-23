c5b87787d06f193be4b239ca84ca00f61c2f431a
Damiano Presa <presadamiano@gmail.com>
2021-03-23 12:48:11 +0100

    hello.hs

--- hello.hs
+++ hello.hs
@@ -1,7 +1,7 @@
-module Main where
-
-import System.Environment (getArgs)
-
-main :: IO ()
-main = do args <- getArgs
-          putStrLn ("Hello: " ++ (args !! 0) ++ ", and " ++ (args !! 1))
+ module Main where
+ import System.Environment
+ 
+ main :: IO ()
+ main = do
+     args <- getArgs
+     putStrLn ("Hello, " ++ args !! 0)
