d58cafaa7bd9960e854eb83ad7bfe195d449eb9f
Damiano Presa <presadamiano@gmail.com>
2021-03-25 20:50:03 +0100

    parser.hs

---
+++ Parsing/parser.hs
@@ -0,0 +1,10 @@
+import Text.ParserCombinators.Parsec () hiding (spaces)
+import System.Environment ()
+
+symbol :: Parser Char 
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String 
+readExpr input = case parse symbol "lisp" input of
+    Left err -> "No match: " ++ show error
+    Right val -> "Found value"
