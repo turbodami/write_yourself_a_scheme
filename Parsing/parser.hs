32d0c8b452abc4e3a50e6bbaa4d526f2e00f11f4
Damiano Presa <presadamiano@gmail.com>
2021-03-31 18:50:54 +0200

    match symbols & blank

--- Parsing/parser.hs
+++ Parsing/parser.hs
@@ -1,10 +1,18 @@
-import Text.ParserCombinators.Parsec () hiding (spaces)
-import System.Environment ()
+import Text.ParserCombinators.Parsec
+import System.Environment
 
 symbol :: Parser Char 
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+spacess :: Parser ()
+spacess = skipMany1 space
+
 readExpr :: String -> String 
-readExpr input = case parse symbol "lisp" input of
-    Left err -> "No match: " ++ show error
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+    Left err -> "No match: " ++ show err
     Right val -> "Found value"
+
+main :: IO ()
+main = do
+          (expr:_) <- getArgs
+          putStrLn (readExpr expr)
